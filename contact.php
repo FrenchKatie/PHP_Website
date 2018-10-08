a23e4600dfb573506ef4682bc3a5411ce09a918b
Katie elizabeth French <845855192@bun2-206-16.local>
2018-10-09 11:53:03 +1300

    php, including header/footer and working with form validation

---
+++ contact.php
@@ -0,0 +1,100 @@
+<?php
+
+    if ($_POST) {
+      // var_dump($_POST);
+      // $name = $_POST["name"];
+      // $email = $_POST["email"];
+      // $message = $_POST["message"];
+      // $subscribe = $_POST["subscribe"];
+
+
+      extract($_POST);
+      // var_dump($name);
+
+      $errors = array();
+
+      //first check if someone has input a value
+      if (!$name){ //if there is not a name
+          array_push($errors, "Name is required!"); //first prop is name of the array pushing to, second prop is what the error actually is
+      } else if (strlen($name) < 2){ //if string length is less than 2
+        array_push($errors, "Please enter at least 2 characters of your name");
+      } else if (strlen($name) > 100){ //if string length is more than 100
+        array_push($errors, "Your name can't be more than 100 characters");
+      }
+
+      if (!$email) {
+        array_push($errors, "Email is required!");
+      } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
+        array_push($errors, "Invalid email");
+      }
+
+      if (!message) {
+        array_push($errors, "Message is requeired");
+      } else if (strlen($message) < 10){ //if string length is less than 2
+        array_push($errors, "Message must be at least 10 chars long");
+      } else if (strlen($message) > 1000){ //if string length is more than 100
+        array_push($errors, "Message cant be more than 1000 chars long");
+      }
+
+
+
+      //spitting out the feedback
+      if(empty($errors)){ //if there are no errors
+        // var_dump("You are good to go");
+        $to = $email;
+        $subject = 'email enquiry';
+        $emailMessage = "You have recieved an email<br>Here is the message";
+        $emailMessage += $message;
+        $headers = array(
+          "From" => "frenchkatie8@gmail.com",
+          "Reply-To" => "frenchkatie8@gmail.com",
+          "X-Mailer" => "PHP/".phpversion()
+        );
+        mail($to,$subject,$emailMessage,$headers);
+        header("Location: index.php");
+      }
+    }
+
+    $page = "fourth";
+    include("templates/header.php");
+    // require();
+ ?>
+  <div class="text-center">
+    <h1 class="header">Contact</h1>
+    <?php if($_POST && !empty($errors)): ?>
+      <div class="alert alert-danger" role="alert">
+          <ul>
+            <?php foreach($errors as $singleError): ?>
+              <li><?= $singleError ?></li>
+            <?php endforeach; ?>
+          </ul>
+      </div>
+    <?php endif; ?>
+
+    <form class="form" action="contact.php" method="post" enctype="multipart/form-data">
+      <div class="form-group">
+        <label for="name">Name</label>
+        <input type="text" class="form-control" name="name" placeholder="Enter your name..." value="<?php if(isset($_POST['name'])){echo $_POST['name'];} ?>">
+      </div>
+      <div class="form-group">
+        <label for="email">Email Address</label>
+        <input type="email" name="email" class="form-control" placeholder="Enter your email address..." value="<?php if(isset($_POST['email'])){echo $_POST['email'];} ?>">
+      </div>
+      <div class="form-group">
+        <label for="message">Message</label>
+        <textarea class="form-control" name="message" rows="3"><?php if(isset($_POST['message'])){echo $_POST['message'];} ?></textarea>
+      </div>
+      <div class="form-group form-check">
+        <input type="checkbox" name="subscribe" class="form-check-input" id="subscribe" <?php echo (isset($_POST['subscribe'])?'checked="checked"':'') ?> />
+        <label class="form-check-label" name="subscribe" for="subscribe">Subscribe to Newsletter</label>
+
+      </div>
+      <button type="submit" class="btn btn-outline-dark btn-block">Submit</button>
+
+    </form>
+
+  </div>
+  <?php
+      include("templates/footer.php");
+      // require();
+   ?>
---
+++ templates/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <title><?= $page ?> page</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<ul class="nav nav-tabs">
+  <li class="nav-item">
+    <a class="nav-link <?php if($page === 'home'): ?> active <?php endif; ?>" href="index.php">Home</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link <?php if($page === 'second'): ?> active <?php endif; ?>" href="second.php">Second</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link <?php if($page === 'third'): ?> active <?php endif; ?>" href="third.php">Third</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link <?php if($page === 'fourth'): ?> active <?php endif; ?>" href="contact.php">Contact</a>
+  </li>
+</ul>
